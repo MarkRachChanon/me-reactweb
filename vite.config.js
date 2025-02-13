1e73328b43c8612d2b9d91153829ce0e7d511d00
MarkHomePro <chanon@nc.ac.th>
2025-02-13 16:08:49 +0700

    redirects

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // ตั้งค่า Base URL ให้เป็น root
-  server: {
-    historyApiFallback: true, // ให้ Dev Server รองรับ History API
-  },
 })
