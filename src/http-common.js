9d16390d1acf8ed7d25dd98c9a020a83dc4f7f4c
MarkHomePro <chanon@nc.ac.th>
2025-02-13 09:13:17 +0700

    update dist

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://me-api-tp1z.onrender.com/",
   headers: {
     "Content-type": "application/json"
   }
