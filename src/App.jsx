fbce24355a691e20562a2a5f5f002b20b22d76bb
MarkHomePro <chanon@nc.ac.th>
2025-02-13 08:56:33 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import User from './Pages/User'
+import Post from './Pages/Post'
+import Profile from './Pages/Profile'
+const App = () => {
+  //สมมุติว่าเรา login แล้ว
+  localStorage.setItem('currentUser', '679b92128289c9e7b70dbb3b');
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/post" element={<Post/>} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
